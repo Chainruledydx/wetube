c548c6c86608bc1f6284afbb99f1e95bae833093
Chainruledydx <60750530+Chainruledydx@users.noreply.github.com>
2020-10-05 13:02:44 +0900

    Router Setting
    express.Router
    AWeSoMe

---
+++ routes.js
@@ -0,0 +1,41 @@
+// 늘 URL 주소를 기억하면서 사용하기엔 버거우니까,
+// 변수로써 만들고 이걸 export해서 변수로 URL에 접근하기로 합시다.
+
+// GLOBAL STUFF
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+//USERS STUFF
+const USERS = "/users";
+const USER_DETAIL = "/:id"; // :id는 가변형 데이터로써 무엇이든 들어갈 수 있다는 의미입니다. ex) /users/1
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+//Videos
+const VIDEOS = "/videos";
+const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id"; // 또한.. :id와 같이 :이 붙은 것을 보면 express가 똒똒해서 '아 이건 가변값이구나'합니다.
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
+
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO,
+};
+
+export default routes;
