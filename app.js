bf1424a8c0d2f27eec3752f0176395689fab34ab
Chainruledydx <60750530+Chainruledydx@users.noreply.github.com>
2020-10-06 17:59:39 +0900

    pug on web
    ohhohoho

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 import express from "express";
-import morgan from "morgan";
 import helmet from "helmet";
-import cookieParser from "cookie-parser";
+import morgan from "morgan";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewears";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
+app.use(helmet()); // 보안관련입니다.
+app.set("view engine", "pug"); // 뷰엔진 - 퍼그 사용
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-app.use(helmet());
+
+app.use(localsMiddleware); // global한 변수를 만들어주는 녀석입니다.
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controller/videoController..js
+++ controller/videoController..js
@@ -1,8 +1,18 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("Search", { pageTitle: "Search", searchingBy });
+};
 
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("videoDetail");
-export const editVideo = (req, res) => res.send("editVideo");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "videos" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "videoDetail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "editVideo" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "deleteVideo" });
